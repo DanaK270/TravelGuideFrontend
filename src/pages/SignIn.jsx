904cacfc47f1cf21e2e19413a60a438180b63cfb
Danak270 <daanaa227700@gmail.com>
2024-10-19 15:58:11 +0300

    progress

---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import { SignInUser } from '../services/Auth'
+import { useNavigate } from 'react-router-dom'
+
+const SignIn = ({ setUser }) => {
+  let navigate = useNavigate()
+  const initState = { email: '', password: '' }
+  const [formValues, setFormValues] = useState({ email: '', password: '' })
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const payload = await SignInUser(formValues)
+    setFormValues(initState)
+    setUser(payload)
+    navigate('/')
+  }
+
+  return (
+    <div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              onChange={handleChange}
+              name="email"
+              type="email"
+              placeholder="example@example.com"
+              value={formValues.email}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              onChange={handleChange}
+              type="password"
+              name="password"
+              value={formValues.password}
+              required
+            />
+          </div>
+          <button disabled={!formValues.email || !formValues.password}>
+            Sign In
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default SignIn
