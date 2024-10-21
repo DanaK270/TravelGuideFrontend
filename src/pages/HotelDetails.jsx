3230ce384b8da7ad2b022d6b2c38443f778cbab5
Danak270 <daanaa227700@gmail.com>
2024-10-21 21:55:56 +0300

    the reviews are now showing! now i must to enable the user to type a review

--- src/pages/HotelDetails.jsx
+++ src/pages/HotelDetails.jsx
@@ -18,40 +18,69 @@ const HotelDetails = () => {
   useEffect(() => {
     getHotel()
   }, [])
+
   const img = `http://localhost:4000/images/${hotel.image}`
+  console.log(hotel.reviews)
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'space-around',
-        marginTop: '3%'
-      }}
-    >
+    <>
       <div
         style={{
-          margin: '3%',
           display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center'
+          justifyContent: 'space-around',
+          marginTop: '3%'
         }}
       >
-        <h1 style={{ fontSize: '4rem' }}>{hotel.name}</h1>
-        <br />
-        <br />
-        {<h2 style={{ fontSize: '2rem' }}>Located in {hotel.country?.name}</h2>}
-        <br />
-        <a
-          target="_blank"
-          href={hotel.link}
-          style={{ textDecoration: 'none', color: 'white', fontSize: '2rem' }}
+        <div
+          style={{
+            margin: '3%',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center'
+          }}
         >
-          View Hotel's Website
-        </a>
+          <h1 style={{ fontSize: '4rem' }}>{hotel.name}</h1>
+          <br />
+          <br />
+          {
+            <h2 style={{ fontSize: '2rem' }}>
+              Located in {hotel.country?.name}
+            </h2>
+          }
+          <br />
+          <a
+            target="_blank"
+            href={hotel.link}
+            style={{ textDecoration: 'none', color: 'white', fontSize: '2rem' }}
+          >
+            View Hotel's Website
+          </a>
+        </div>
+        <div>
+          <img src={img} alt="img" />
+        </div>
       </div>
-      <div>
-        <img src={img} alt="img" />
+
+      <div style={{ marginTop: '5%' }}>
+        <ul className="countries-list">
+          <h2 style={{ marginLeft: '2%' }}>Reviews</h2>
+          {hotel.reviews?.map((review) => (
+            <li
+              key={review.id}
+              className="country-item"
+              style={{
+                margin: '10px',
+                display: 'flex',
+                justifyContent: 'space-between'
+              }}
+            >
+              <h4>{review.comment} </h4>
+              <h4>{review.score} / 5</h4>
+            </li>
+          ))}
+        </ul>
       </div>
-    </div>
+    </>
   )
 }
 
