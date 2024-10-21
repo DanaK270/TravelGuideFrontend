6b5187da67ffedbf4c7046cb9d19edff37f29d9c
IsaAlEid <isa.al.eid@gmail.com>
2024-10-21 09:21:24 +0300

    commented out stripe

--- src/components/BookFlight.jsx
+++ src/components/BookFlight.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
-import StripeWrapper from './StripePayment';
+// import StripeWrapper from './StripePayment';
 
 const BookFlight = () => {
   const [flightDetails, setFlightDetails] = useState({
