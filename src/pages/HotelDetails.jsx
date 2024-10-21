a7a7491c171e78cb6c001dcec5c4718c137a2c9e
Danak270 <daanaa227700@gmail.com>
2024-10-21 22:26:50 +0300

    add review done

--- src/pages/HotelDetails.jsx
+++ src/pages/HotelDetails.jsx
@@ -5,6 +5,7 @@ import { useParams } from 'react-router-dom'
 const HotelDetails = () => {
   const { id } = useParams()
   const [hotel, setHotel] = useState('')
+  const [reviewData, setReviewData] = useState({ comment: '', score: 0 })
 
   const getHotel = async () => {
     try {
@@ -19,8 +20,36 @@ const HotelDetails = () => {
     getHotel()
   }, [])
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const token = localStorage.getItem('token') //to get the user
+      const review = {
+        comment: reviewData.comment,
+        score: reviewData.score,
+        hotel: id
+      }
+
+      await Client.post('/review/add', review, {
+        headers: {
+          //we need to add this header because it is a protected route
+          Authorization: `Bearer ${token}`
+        }
+      })
+
+      //get hotel again to get the updated data (with the newly added review!)
+      getHotel()
+      setReviewData({ comment: '', score: 0 })
+    } catch (err) {
+      console.error('Error submitting review:', err)
+    }
+  }
+
+  const handleChange = (e) => {
+    setReviewData({ ...reviewData, [e.target.name]: e.target.value })
+  }
+
   const img = `http://localhost:4000/images/${hotel.image}`
-  console.log(hotel.reviews)
 
   return (
     <>
@@ -79,6 +108,41 @@ const HotelDetails = () => {
             </li>
           ))}
         </ul>
+      </div>
+
+      <div style={{ marginTop: '5%', padding: '1%' }}>
+        <form onSubmit={handleSubmit}>
+          <h3>Leave a Review</h3>
+          <br />
+          <div>
+            <textarea
+              name="comment"
+              value={reviewData.comment}
+              onChange={handleChange}
+              required
+              rows="3"
+              style={{ width: '100%', marginBottom: '1rem' }}
+              placeholder={`what do you think about ${hotel.name} ?`}
+            />
+          </div>
+          <div>
+            <label>Rate Your Experience at {hotel.name}!</label>
+            <br />
+            <input
+              className="input"
+              style={{ height: '30px', width: '200px' }}
+              type="number"
+              name="score"
+              value={reviewData.score}
+              onChange={handleChange}
+              min="1"
+              max="5"
+              required
+            />
+          </div>
+          <br />
+          <button type="submit">Submit Review</button>
+        </form>
       </div>
     </>
   )
